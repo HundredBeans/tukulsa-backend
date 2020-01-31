7dd15a5a8e15a1d797abb571eb20a0a853aeebef
Mohammad Daffa <daffa@alterra.id>
2020-01-31 13:00:09 +0700

    change port

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ if __name__ == '__main__':
         logging.getLogger().setLevel(logging.INFO)
         log_handler.setFormatter(formatter)
         app.logger.addHandler(log_handler)
-        app.run(debug=False, host='0.0.0.0', port=5000)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)

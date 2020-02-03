cb6172586ba61fbfd78b2feebf3d261d33d60609
Achmad Miftahul Ulum <miftahul@alterra.id>
2020-02-03 10:17:20 +0700

    add new table chat

--- blueprints/transactions/models.py
+++ blueprints/transactions/models.py
@@ -2,36 +2,43 @@ from blueprints import db
 from flask_restful import fields
 from datetime import datetime
 
+
 class Product(db.Model):
-  __tablename__= "product"
-  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-  product_id_kraken = db.Column(db.Integer, nullable= False)
-  image_path = db.Column(db.String(255), default= None)
-  
+    __tablename__ = "product"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_id_api = db.Column(db.Integer, nullable=False)
+    image_path = db.Column(db.String(1000), default=None)
+
+
 class Transactions(db.Model):
-  __tablename__= "transactions"
-  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-  product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable= False)
-  trx_id_kraken = db.Column(db.Integer, nullable = False, default= 0)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable= False)
-  provider =db.Column(db.String(10), nullable = False, default= None)
-  phone_number =db.Column(db.String(13), nullable = False)
-  label = db.Column(db.String(100), nullable = False , default= None)
-  nominal = db.Column(db.Integer, nullable = False)
-  price = db.Column(db.Integer, nullable = False)
-  created_at = db.Column(db.DateTime, nullable = False)
-  payment_status = db.Column(db.Boolean, default = False)
-  status = db.Column(db.Boolean, default = False)
-  trx_product = db.relationship('product', backref= 'transactions')
-  trx_timedetail = db.relationship('timedetails', backref= 'transactions')
+    __tablename__ = "transactions"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'product.id'), nullable=False)
+    trx_id_api = db.Column(db.Integer, nullable=False, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id', ondelete='CASCADE'), nullable=False)
+    provider = db.Column(db.String(10), nullable=False, default=None)
+    phone_number = db.Column(db.String(13), nullable=False)
+    label = db.Column(db.String(255), nullable=False, default=None)
+    nominal = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    payment_status = db.Column(db.Boolean, default=False)
+    status = db.Column(db.Boolean, default=False)
+    trx_product = db.relationship('product', backref='transactions')
+    trx_timedetail = db.relationship('timedetails', backref='transactions')
+
 
 class Timedetails(db.Model):
-  __tablename__= "timedetails"
-  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-  time_trxid = db.Column(db.Integer, db.ForeignKey('transactions.id', ondelete='CASCADE'), nullable= False)
-  time_userid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
-  year = db.Column(db.Integer, nullable= False, default= None)
-  month = db.Column(db.Integer, nullable= False, default= None)
-  date = db.Column(db.Integer, nullable= False, default= None)
-  month_char = db.Column(db.String(15), default = None )
-  date_char = db.Column(db.String(10), default= None)
+    __tablename__ = "timedetails"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    time_trxid = db.Column(db.Integer, db.ForeignKey(
+        'transactions.id', ondelete='CASCADE'), nullable=False)
+    time_userid = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    year = db.Column(db.Integer, nullable=False, default=None)
+    month = db.Column(db.Integer, nullable=False, default=None)
+    date = db.Column(db.Integer, nullable=False, default=None)
+    month_char = db.Column(db.String(15), default=None)
+    date_char = db.Column(db.String(10), default=None)
--- blueprints/users/models.py
+++ blueprints/users/models.py
@@ -2,22 +2,33 @@ from blueprints import db
 from flask_restful import fields
 from datetime import datetime
 
+
 class Users(db.Model):
-  __tablename__= "users"
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  line_id = db.Column(db.String(255), nullable=False)
-  display_name = db.Column(db.String(255), nullable=False)
-  user_status = db.Column(db.String(255), nullable=False)
-  user_transactions = db.relationship('transactions', backref='users', lazy='dynamic')
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    line_id = db.Column(db.String(255), nullable=False)
+    display_name = db.Column(db.String(255), nullable=False)
+    user_status = db.Column(db.String(255), nullable=False)
+    user_transactions = db.relationship(
+        'transactions', backref='users', lazy='dynamic')
+    user_chat = db.relationship('chat', backref='users')
 
-  response_fileds = {
-    'id' : fields.Integer,
-    'display_name' : fields.String,
-    'line_id' : fields.String,
-    'user_status' : fields.String
-  }
+    response_fileds = {
+        'id': fields.Integer,
+        'display_name': fields.String,
+        'line_id': fields.String,
+        'user_status': fields.String
+    }
 
-  def __init__(self, line_id, display_name, user_status):
-    self.line_id = line_id
-    self.display_name = display_name
-    self.user_status = user_status
+    def __init__(self, line_id, display_name, user_status):
+        self.line_id = line_id
+        self.display_name = display_name
+        self.user_status = user_status
+
+
+class Chat(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    chat_userid = db.Column(db.Integer, db.ForeignKey(
+        'users.id', ondelete='CASCADE'), nullable=False)
+    status = db.Column(db.Boolean, default=False)

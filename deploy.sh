9fa1b1414c12a016ad9bf2cfcf3ebcbcb979fa94
Mohammad Daffa <daffa@alterra.id>
2020-02-07 01:46:57 +0700

    edit deploy sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git pull
 mkdir storage && cd storage && mkdir logs
 
 source ~/.profile
-echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
 docker stop tukulsaBE
 docker rm tukulsaBE
 docker rmi daffa99/containerd:tukulsaBE

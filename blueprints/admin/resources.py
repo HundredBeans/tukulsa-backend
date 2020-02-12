e0dfaa99b4da77e6b4a13d79f1573092b878a3de
ilham faturachman <faturachman@alterra.id>
2020-02-12 20:27:36 +0700

    new get balance and total transaction13

--- blueprints/admin/resources.py
+++ blueprints/admin/resources.py
@@ -165,6 +165,7 @@ class AdminGetTransactionId(Resource):
 class AdminGetTransactionList(Resource):
   # @jwt_required
   def get(self):
+      parser=reqparse.RequestParser()
       parser.add_argument('line_id', location='json', required=True)
       parser.add_argument('order_id', location='json')
       parser.add_argument('page', location='args', default=1)
